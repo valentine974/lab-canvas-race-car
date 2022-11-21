a564427950353e109e1663d9f57b31efb03bea8e
valentine974 <valentine.bathany@hotmail.fr>
2022-11-21 21:17:53 +0100

    succeded in adding crashing

--- js/index.js
+++ js/index.js
@@ -9,10 +9,15 @@ const myGameArea = {
     this.roadImg.src = "../images/road.png";
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.context.drawImage( this.roadImg, 0, 0, this.canvas.width, this.canvas.height );
+    this.interval = setInterval(updateGameArea, 20);
   },
   clear: function () {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.context.drawImage( this.roadImg, 0, 0, this.canvas.width, this.canvas.height );
+  },
+  
+  stop: function () {
+    clearInterval(this.interval);
   },
 };
 
@@ -35,6 +40,19 @@ class Component {
     this.x += this.speedX; 
   }
 
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+
   crashWith(obstacle) {
     return !(this.bottom() < obstacle.top() || this.top() > obstacle.bottom() || this.right() < obstacle.left() || this.left() > obstacle.right());
   }
@@ -53,6 +71,27 @@ class Obstacles {
     const ctx = myGameArea.context;
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  newPos() {
+    this.x += this.speedX; 
+  }
+  
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+
+  crashWith(obstacle) {
+    return !(this.bottom() < obstacle.top() || this.top() > obstacle.bottom() || this.right() < obstacle.left() || this.left() > obstacle.right());
   }
 }
 
@@ -84,63 +123,62 @@ const carY = 400;
 const car = new Component(carW, carH, carImg, carX, carY);
 
 
+function updateGameArea() {
+  myGameArea.clear();
+  car.newPos();
+  car.update();
+  updateObstacles(); 
+  checkGameOver();
+}
+
+function updateObstacles() {
+  for (i = 0; i < myObstacles.length; i++) {
+    myObstacles[i].y += 1;
+    myObstacles[i].update(); 
+    console.log(myObstacles)
+  }
+
+  myGameArea.frames += 1;
+
+  if (myGameArea.frames % 180 === 0) {
+    // let y = myGameArea.canvas.height;
+    let y = 0
+    let minWidth = 20;
+    let maxWidth = 300;
+    let width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
+    let minGap = 100;
+    let maxGap = 200;
+    let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+    myObstacles.push(new Obstacles( width                                , 20, '#870107', 0          , y));
+    myObstacles.push(new Obstacles( myGameArea.canvas.width - width - gap, 20, '#870107', width + gap, y));
+  }
+}
+
+function checkGameOver() {
+  const crashed = myObstacles.some(function (obstacle) {
+    return car.crashWith(obstacle);
+  });
+ 
+  if (crashed) {
+    myGameArea.stop();
+  }
+ 
+}
+
+
 window.onload = () => {
   document.getElementById("start-button").onclick = () => {
     startGame();
   };
-  let interval
-  function startGame() {
-    myGameArea.start(); 
-    interval = setInterval(updateGameArea, 20);
 
+  function startGame() {
+    myGameArea.start();  
+  
      
   }
-
-  
  
-  function updateGameArea() {
-    myGameArea.clear();
-    car.newPos();
-    car.update();
-    updateObstacles();
-    checkGameOver();
-  }
 
-  function updateObstacles() {
-    for (i = 0; i < myObstacles.length; i++) {
-      myObstacles[i].y += 1;
-      myObstacles[i].update(); 
-    }
 
-    myGameArea.frames += 1;
-
-    if (myGameArea.frames % 180 === 0) {
-      // let y = myGameArea.canvas.height;
-      let y = 0
-      let minWidth = 20;
-      let maxWidth = 300;
-      let width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
-      let minGap = 100;
-      let maxGap = 200;
-      let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
-      myObstacles.push(new Obstacles( width                                , 20, '#870107', 0          , y));
-      myObstacles.push(new Obstacles( myGameArea.canvas.width - width - gap, 20, '#870107', width + gap, y));
-    }
-  }
-
-  function stopGame(){
-    clearInterval(interval);
-
-  }
-
-  function checkGameOver() {
-    const crashed = myObstacles.some(function (obstacle) {
-      return player.crashWith(obstacle);
-    });
-   
-    if (crashed) {
-      stopGame()
-    }
-  }
+ 
 
 };
